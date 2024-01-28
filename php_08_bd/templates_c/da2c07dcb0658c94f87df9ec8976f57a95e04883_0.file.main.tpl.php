8b2fb27a298b8ee961d797c9c4b097e090e5b615
KarolinaKula1998 <152809183+KarolinaKula1998@users.noreply.github.com>
2024-01-28 16:18:58 +0100

    Add files via upload

---
+++ php_08_bd/templates_c/da2c07dcb0658c94f87df9ec8976f57a95e04883_0.file.main.tpl.php
@@ -0,0 +1,140 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-01-28 15:32:50
+  from "C:\Users\karol\OneDrive\Pulpit\Aplikacje Sieciowe\htdocs\php_08_bd\app\views\templates\main.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_65b6659279f160_58567067',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'da2c07dcb0658c94f87df9ec8976f57a95e04883' => 
+    array (
+      0 => 'C:\\Users\\karol\\OneDrive\\Pulpit\\Aplikacje Sieciowe\\htdocs\\php_08_bd\\app\\views\\templates\\main.tpl',
+      1 => 1706301961,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_65b6659279f160_58567067 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Aplikacja bazodanowa</title>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+		integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/style.css">
+</head>
+
+<body style="margin: 20px;">
+
+<div class="pure-menu pure-menu-horizontal bottom-margin">
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+personList" class="pure-menu-heading pure-menu-link">Lista</a>
+<?php if (count($_smarty_tpl->tpl_vars['conf']->value->roles) > 0) {?>
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+logout" class="pure-menu-heading pure-menu-link">Wyloguj</a>
+<?php } else { ?>	
+	<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+loginShow" class="pure-menu-heading pure-menu-link">Zaloguj</a>
+<?php }?>
+</div>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_29412971865b66592791dc4_91846354', 'top');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_71710780365b6659279cee8_62911392', 'messages');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_119635735665b6659279e7a9_18834415', 'bottom');
+?>
+
+
+</body>
+
+</html><?php }
+/* {block 'top'} */
+class Block_29412971865b66592791dc4_91846354 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ <?php
+}
+}
+/* {/block 'top'} */
+/* {block 'messages'} */
+class Block_71710780365b6659279cee8_62911392 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+<div class="messages error bottom-margin">
+	<ul>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ul>
+</div>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+<div class="messages info bottom-margin">
+	<ul>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ul>
+</div>
+<?php }?>
+
+<?php
+}
+}
+/* {/block 'messages'} */
+/* {block 'bottom'} */
+class Block_119635735665b6659279e7a9_18834415 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ <?php
+}
+}
+/* {/block 'bottom'} */
+}
