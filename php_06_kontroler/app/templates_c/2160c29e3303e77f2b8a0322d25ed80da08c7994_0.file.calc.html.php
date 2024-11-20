a641dbdefcebc2c99e37d906f2790f97664c4fc6
KarolinaKula1998 <152809183+KarolinaKula1998@users.noreply.github.com>
2024-11-20 19:44:52 +0100

    Add files via upload

---
+++ php_06_kontroler/app/templates_c/2160c29e3303e77f2b8a0322d25ed80da08c7994_0.file.calc.html.php
@@ -0,0 +1,136 @@
+<?php
+/* Smarty version 4.5.4, created on 2024-11-20 19:17:52
+  from 'C:\xampp\htdocs\php_06_kontroler\app\calc\calc.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.4',
+  'unifunc' => 'content_673e27d02cd480_60372745',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2160c29e3303e77f2b8a0322d25ed80da08c7994' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_06_kontroler\\app\\calc\\calc.html',
+      1 => 1732126566,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_673e27d02cd480_60372745 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_366802517673e27d02bce01_57754719', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_906305842673e27d02be297_16143281', 'content');
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, ($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.html"));
+}
+/* {block 'footer'} */
+class Block_366802517673e27d02bce01_57754719 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_366802517673e27d02bce01_57754719',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_906305842673e27d02be297_16143281 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_906305842673e27d02be297_16143281',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Kalkulator kredytowy</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute#app_content" method="post">
+		<fieldset>
+		<label for="kwota">Kwota kredytu o którą się ubiegasz </label>
+		<input id="kwota" type="text" placeholder="kwota" name="kwota" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->kwota;?>
+">
+		<label for="oprocentowanie">Oprocentowanie kredytu w % </label>
+		<input id="oprocentowanie" type="text" placeholder="oprocentowanie" name="oprocentowanie" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->oprocentowanie;?>
+">
+		<label for="okres">Czas kredytowania w miesiącach </label>
+		<input id="okres" type="text" placeholder="okres" name="okres" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->okres;?>
+">
+		<button type="submit" class="pure-button pure-button-primary">Oblicz</button>
+		</fieldset>	
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Wystąpiły błędy: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+$_smarty_tpl->tpl_vars['err']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+$_smarty_tpl->tpl_vars['err']->do_else = false;
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+	</ol>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Informacje: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+$_smarty_tpl->tpl_vars['inf']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+$_smarty_tpl->tpl_vars['inf']->do_else = false;
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+	</ol>
+<?php }?>
+
+<?php if ((isset($_smarty_tpl->tpl_vars['result']->value->rata))) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['result']->value->rata;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
